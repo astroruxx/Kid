7dd8716894d6c805b51d9fc80acd0d1650174457
astro <hawkeyerenner4@gmail.com>
2022-03-15 17:20:27 -0500

    Update kick.js

--- commands/moderation/ban/kick.js
+++ commands/moderation/ban/kick.js
@@ -3,7 +3,7 @@ const {
     Message,
     MessageEmbed
 } = require('discord.js');
-
+const schema = require('../../../model/logs')
 module.exports = {
     name: 'kick',
     /** 
@@ -12,6 +12,10 @@ module.exports = {
      * @param {String[]} args 
      */
     run: async (client, message, args, Discord) => {
+        const data = await schema.findOne({ GuildId: message.guild.id });
+        const channelaa = await client.channels.fetch(data.ChannelId);
+        const channelsend = client.channels.fetch(channelaa.id).then(channel => channel.send({embeds: [embed]}))
+        if(!channelsend) return message.reply('Please setup a log channel for the bot to work properly')
         const reason = args.slice(1).join(' ');
         const user = message.mentions.members.first();
         const embed = new MessageEmbed()
@@ -23,6 +27,7 @@ module.exports = {
         if (user) {
             user.kick().then(() => {
                 message.channel.send({embeds: [embed]})
+                channelsend
             })
         } else {
             message.channel.send('Error member is unknown')
